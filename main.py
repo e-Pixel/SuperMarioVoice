8925ece5bcb4f4a387762cf2d781cc2a5dd0adc5
autum <ipixelsun@gmail.com>
2022-04-11 13:02:06 -0700

    Setting up

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import speech_recognition as sr
+
+print(sr.__version__)
+r = sr.Recognizer()
+# r.recognize_google()
+# This will give an error, we need to record some sort of audio for it to work. 
+
+
+
 print("hello world")
-# :D
-# hopefully this will work without any big problems behind 
-# aaaaaaaaaaa
